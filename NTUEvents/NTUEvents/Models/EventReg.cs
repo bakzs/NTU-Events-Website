d56d5efe69caa4d3e17905a3478e01297f5c65d0
Jiawei Tan <JiaweiTan@users.noreply.github.com>
2018-12-08 18:03:47 +0800

    Add Class Files

---
+++ NTUEvents/NTUEvents/Models/EventReg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTUEvents.Models
+{
+    public class EventReg
+    {
+        public int EventRegId { get; set; }
+        public int EventId { get; set; }
+        public int UserId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Boolean IsDeleted { get; set; }
+    }
+}
---
+++ NTUEvents/NTUEvents/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTUEvents.Models
+{
+    public class UserProfile
+    {
+        public int UserProfileId { get; set; }
+        public string Name { get; set; }
+        public int Contact { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public Boolean isDeleted { get; set; }
+    }
+}
