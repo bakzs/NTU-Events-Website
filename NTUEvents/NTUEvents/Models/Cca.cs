e6fa9e71be7dce1b7d516b6dfbacda7157c28760
leeweiminsg <40821628+leeweiminsg@users.noreply.github.com>
2018-12-08 18:20:49 +0800

    Add Class Files

---
+++ NTUEvents/NTUEvents/Models/Cca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTUEvents.Models
+{
+    public class Cca
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Schedule { get; set; }
+        public string Venue { get; set; }
+        public string Contact { get; set; }
+        public int MemberId { get; set; }
+    }
+}
---
+++ NTUEvents/NTUEvents/Models/CcaMembership.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTUEvents.Models
+{
+    public class CcaMembership
+    {
+        public int CcaMembershipId { get; set; }
+        public int UserId { get; set; }
+        public int CcaId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
