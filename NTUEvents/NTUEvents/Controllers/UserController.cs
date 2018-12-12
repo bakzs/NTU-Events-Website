e0d01c8256cf73c1056fd8888d88376eaeeb8e0d
leeweiminsg <40821628+leeweiminsg@users.noreply.github.com>
2018-12-12 22:01:58 +0800

    Complete UserController
    Implemented CRUD

--- NTUEvents/NTUEvents/Controllers/UserController.cs
+++ NTUEvents/NTUEvents/Controllers/UserController.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using NTUEvents.Models;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace NTUEvents.Controllers
 {
+    /* 
+        Method   Functions              Routing 
+     1. [Get]    GetUser         - api/user/{userId}
+     3. [Post]   CreateUser      - api/user/create/
+     4. [Put]    UpdateUser      - api/user/update/{userId}
+     5. [Delete] DeleteUser      - api/user/delete/{userId}
+
+     *Note
+     0. Do not key incorrect userId. 
+     1. When testing on postman, select the correct method
+     2. For [Put] method, type "Content-type" into Key and "application/json" into Value
+     3. Generate the Json string according to the Object values
+     4. Insert Json string into Description.
+
+    */
+
     [Route("api/[controller]")]
     public class UserController : Controller
     {
@@ -22,37 +37,38 @@ namespace NTUEvents.Controllers
         [HttpGet("{id:int}")]
         public IActionResult GetUser(int userId)
         {
-            //Return user profile
-
-            //Return a view object
-            return RedirectToAction("Index");
+            var user = NtuEventsContext.User.Find(userId);
+            var userProfileJson = JsonConvert.SerializeObject(user);
+            return Json(userProfileJson);
         }
 
-        [HttpPost]
+        [HttpPost("create")]
         public IActionResult CreateUser([FromBody] User UserItem)
         {
-            //Create user profile
-
-            //Return a view object
-            return RedirectToAction("Index");
+            NtuEventsContext.User.Add(UserItem);
+            NtuEventsContext.SaveChanges();
+            return Ok("You have successfully created your account!");
         }
 
-        [HttpPut("{id:int}")]
-        public IActionResult UpdateUser(int userId)
+        [HttpPut("update/{id:int}")]
+        public IActionResult UpdateUser([FromBody] User UserItem, int userId)
         {
-            //Update user profile
-
-            //Return a view object
-            return RedirectToAction("Index");
+            var user = NtuEventsContext.User.Find(userId);
+            user.Username = UserItem.Username;
+            user.Password = UserItem.Password;
+            user.Cca = UserItem.Cca;
+            user.CcaMembership = UserItem.CcaMembership;
+            user.Events = UserItem.Events;
+            return Ok("You have successfully updated your account!");
         }
 
-        [HttpDelete("{id:int}")]
+        [HttpDelete("delete/{id:int}")]
         public IActionResult DeleteUser(int userId)
         {
-            //Delete user profile
-
-            //Return a view object
-            return RedirectToAction("Index");
+            var user = NtuEventsContext.User.Find(userId);
+            user.IsDeleted = true;
+            NtuEventsContext.SaveChanges();
+            return Ok("You have successfully deleted your account!");
         }
     }
 }
