7ec787b537825801d8c0f25b3b81191d9c4f7956
Jiawei Tan <JiaweiTan@users.noreply.github.com>
2018-12-22 16:19:03 +0800

    Basic Vue Structure

---
+++ Vue App/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+      <b-navbar toggleable="md" type="dark" variant="info">
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand href="#">NTUEvents</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav>
+            <b-nav-item to="/main">Home</b-nav-item>
+            <b-nav-item to="/events">Events</b-nav-item>
+            <b-nav-item to="/user">User</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items
+          <b-navbar-nav class="ml-auto">
+          </b-navbar-nav>-->
+        </b-collapse>
+      </b-navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MainPage from "./components/main";
+
+export default {
+  name: "app",
+  components: {
+    MainPage
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Vue App/src/components/user.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="user">
+    <h1>This is user</h1>
+  </div>
+</template>
+
+<style>
+</style>
