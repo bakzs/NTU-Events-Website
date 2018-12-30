1788bf065f0850acf9466ad423a3e3512397ec01
leeweiminsg <40821628+leeweiminsg@users.noreply.github.com>
2018-12-30 14:59:13 +0800

    Edit main page

--- Vue App/src/components/main.vue
+++ Vue App/src/components/main.vue
@@ -1,387 +1,90 @@
 <template>
-  <div class="main">
-    <div class="container">
-      <div
-        class="modal fade"
-        id="toploginModal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="toploginModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header bg-primary text-white">
-              <h5 class="modal-title" id="toploginModalLabel">Login</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-              </button>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-carousel id="carousel1"
+                      controls
+                      indicators
+                      img-width="1024"
+                      img-height="480"
+          >
+            <b-carousel-slide caption="Test Cca"
+                              text="Test Text"
+                              img-src="https://picsum.photos/1024/480/?image=52"
+            ></b-carousel-slide>
+          </b-carousel>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+        <b-card-group deck>
+          <b-card title="Test Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Test Image" img-top border-variant="light" class="text-center">
+            <p>Test Text</p>
+            <b-button>Test Button</b-button>
+            <div slot="footer">
+                <small class="text-muted">By: Test Cca</small>
             </div>
-            <div class="modal-body">
-              <form class="form-horizontal">
-                <div class="form-group">
-                  <input
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    class="form-control"
-                    required="required"
-                    type="text"
-                  >
-                </div>
-                <div class="form-group">
-                  <input
-                    id="password"
-                    name="password"
-                    placeholder="password"
-                    class="form-control"
-                    type="text"
-                  >
-                </div>
-                <div class="form-group">
-                  <button name="submit" type="submit" class="btn btn-primary">Login</button>
-                  <button type="button" class="btn btn-link">Forgot Password?</button>
-                </div>
-              </form>
+          </b-card>
+          <b-card title="Test Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Test Image" img-top border-variant="light" class="text-center">
+            <p>Test Text</p>
+            <b-button>Test Button</b-button>
+            <div slot="footer">
+                <small class="text-muted">By: Test Cca</small>
             </div>
-            <div class="modal-footer">
-              <small>New to Refers ?
-                <a href="#">Register here</a>
-              </small>
+          </b-card>
+          <b-card title="Test Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Test Image" img-top border-variant="light" class="text-center">
+            <p>Test Text</p>
+            <b-button>Test Button</b-button>
+            <div slot="footer">
+                <small class="text-muted">By: Test Cca</small>
             </div>
-          </div>
-        </div>
-      </div>
-      <div class="mt-5"></div>
-      <section class="aboutus py-5" id="aboutus">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-3">
-              <div class="card">
-                <div class>
-                  <h4 class="pl-3 py-2">Categories</h4>
-                  <ul class="list-unstyled">
-                    <li>
-                      <a href="dashboard.html" class="list-group-item bg-active">
-                        <i class="fa fa-tachometer"></i> Dashboard
-                        <span class="badge">12</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="my-profile.html" class="list-group-item">
-                        <i class="fa fa-user"></i> My Profile
-                      </a>
-                    </li>
-                    <li>
-                      <a href="my-listings.html" class="list-group-item">
-                        <i class="fa fa-list"></i> My Listings
-                      </a>
-                    </li>
-                    <li>
-                      <a href="my-enquiry.html" class="list-group-item">
-                        <i class="fa fa-envelope"></i> My Enquiry
-                      </a>
-                    </li>
-                    <li>
-                      <a href="add-business.html" class="list-group-item">
-                        <i class="fa fa-plus-square"></i> Add Business
-                      </a>
-                    </li>
-                    <li>
-                      <a href="login.html" class="list-group-item">
-                        <i class="fa fa-key"></i> Login
-                      </a>
-                    </li>
-                    <li>
-                      <a href="signup.html" class="list-group-item">
-                        <i class="fa fa-user-plus"></i> Signup
-                      </a>
-                    </li>
-                    <li>
-                      <a href="account-settings.html" class="list-group-item">
-                        <i class="fa fa-wrench"></i> Account Settings
-                      </a>
-                    </li>
-                    <li>
-                      <a href="change-password.html" class="list-group-item">
-                        <i class="fa fa-lock"></i> Change Password
-                      </a>
-                    </li>
-                    <li>
-                      <a href="listings-page.html" class="list-group-item">
-                        <i class="fa fa-angle-double-right"></i> Listings Page
-                      </a>
-                    </li>
-                    <li>
-                      <a href="product-page.html" class="list-group-item">
-                        <i class="fa fa-angle-double-right"></i> Product Page
-                      </a>
-                    </li>
-                    <li>
-                      <a href="logout.html" class="list-group-item">
-                        <i class="fa fa-power-off"></i> Log Out
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-9">
-              <div class="card pb-5">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-md-12">
-                      <h5>Free Responsive Mobile Website Templates (3147)</h5>
-                      <div class="pb-3"></div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/05/envy_boutique_Free17-01-2018_1395693613.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/01/campus_Free10-01-2018_434855747.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>School</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/04/zoom_lab_Free22-02-2018_1592802516.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="row mt-4">
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/05/envy_boutique_Free17-01-2018_1395693613.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/01/campus_Free10-01-2018_434855747.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>School</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/04/zoom_lab_Free22-02-2018_1592802516.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="row mt-4">
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/05/envy_boutique_Free17-01-2018_1395693613.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/01/campus_Free10-01-2018_434855747.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>School</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/04/zoom_lab_Free22-02-2018_1592802516.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="row mt-4">
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/05/envy_boutique_Free17-01-2018_1395693613.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/01/campus_Free10-01-2018_434855747.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>School</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/04/zoom_lab_Free22-02-2018_1592802516.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="row mt-4">
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/05/envy_boutique_Free17-01-2018_1395693613.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/01/campus_Free10-01-2018_434855747.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>School</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                    <div class="col-md-4">
-                      <a href="#">
-                        <div class="card text-center">
-                          <img
-                            class="card-img-top"
-                            src="https://w3layouts.com/wp-content/uploads/2018/04/zoom_lab_Free22-02-2018_1592802516.jpg"
-                            alt=" "
-                          >
-                          <div class="card-body">
-                            <h5>Catchy</h5>
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                  <div class="row mt-4">
-                    <div class="col-md-12">
-                      <ul class="pagination">
-                        <li class="page-item">
-                          <a class="page-link" href="#">Previous</a>
-                        </li>
-                        <li class="page-item">
-                          <a class="page-link" href="#">1</a>
-                        </li>
-                        <li class="page-item">
-                          <a class="page-link" href="#">2</a>
-                        </li>
-                        <li class="page-item">
-                          <a class="page-link" href="#">3</a>
-                        </li>
-                        <li class="page-item">
-                          <a class="page-link" href="#">Next</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+          </b-card>
+        </b-card-group>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-card title="Card Title">
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional content.
+            </p>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data(){
+    return{
+      allEvents: {}
+    };
+  },
+  methods: {
+    participateInEvent() {
+
+    }
+  },
+  mounted() {
+    var self = this;
+    this.axios.get("https://localhost:44362/api/events")
+      .then(function (response) {
+      console.log(response);
+      this.allEvents = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
